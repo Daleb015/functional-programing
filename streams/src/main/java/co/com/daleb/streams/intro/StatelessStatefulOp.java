ccd999033dd678488e4941d1a35c6e1b9ec76323
Daleb015 <daleb015@gmail.com>
2021-06-19 23:24:49 -0500

    stateful and stateless operations

---
+++ streams/src/main/java/co/com/daleb/streams/intro/StatelessStatefulOp.java
@@ -0,0 +1,17 @@
+package co.com.daleb.streams.intro;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StatelessStatefulOp {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9);
+
+        // this is a bad use of parallel because skip and limit are statefull operations
+        List<Integer> collect = list
+                .parallelStream()
+                .skip(2)
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+}
