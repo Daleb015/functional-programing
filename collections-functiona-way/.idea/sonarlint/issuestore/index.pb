28d925d85ca4414c8d6a38d252f3a1717f84358f
Daleb015 <daleb015@gmail.com>
2021-07-03 13:09:50 -0500

    Collections and map functional way

--- collections-functiona-way/.idea/sonarlint/issuestore/index.pb
+++ collections-functiona-way/.idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,18 @@
 
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+T
+$src/main/java/org/example/Theory.txt,a\9\a9ae8130a7978c718f129a53ec825d120aa86707
+g
+7src/main/java/org/example/ListFunctionalOperations.java,b\b\bbd7c259be960af6086721fbe5f885ad5b8c053b
+T
+$src/main/java/org/example/Movie.java,1\d\1d072492392425e32cba576919b21272112d041a
 9
 	.dcignore,7\7\7791000a9a93019a87d67e0b676c86447ba29c67
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+R
+"src/main/java/org/example/App.java,2\6\268b09d2a5cdab7d148f0ec91f7b3fb51601410d
+V
+&src/test/java/org/example/AppTest.java,4\3\438c56474209176629917f1aaaac4e1cf31f765e
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ collections-functiona-way/src/main/java/org/example/MapFunctionalOperations.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MapFunctionalOperations {
+    public static void main(String[] args) {
+
+        Map<String, String> contacts = new HashMap<>();
+        contacts.put("1234654987","Jhon");
+        contacts.put("1237009020","John");
+        contacts.put("7890291111","Neal");
+        contacts.put("2647210290","Raju");
+        contacts.put("9999999999","Peter");
+        contacts.put("9081234567","Neha");
+
+        // Traversal
+        //before java 8
+        for (Map.Entry<String,String> entry : contacts.entrySet()){
+            System.out.println(entry.getKey()+" - "+entry.getValue());
+        }
+        System.out.println("----------------------------------------");
+
+        contacts.forEach((k,v) -> System.out.println(k+" - "+v));
+        System.out.println("----------------------------------------");
+
+        // Filter
+
+        contacts.entrySet()
+                .stream()
+                .filter(contact -> "John".equalsIgnoreCase(contact.getValue()))
+                .forEach(System.out::println);
+        System.out.println("----------------------------------------");
+
+        Map<String, String> collect = contacts.entrySet()
+                .stream()
+                .filter(contact -> "John".equalsIgnoreCase(contact.getValue()))
+                .collect(Collectors.toMap(c -> c.getKey(), c -> c.getValue()));
+        System.out.println("----------------------------------------");
+
+        // Map
+
+        String collect1 = contacts.entrySet()
+                .stream()
+                .distinct()
+                .map(c -> c.getValue())
+                .collect(Collectors.joining(" , "));
+
+        System.out.println(collect1);
+        System.out.println("----------------------------------------");
+
+        // Sorting
+
+        LinkedHashMap<String, String> sortedMap = contacts.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(c -> c.getKey(), c -> c.getValue(), (v1, v2) -> v1, LinkedHashMap::new));
+
+        sortedMap.forEach((k,v) -> System.out.println(k+" - "+v));
+
+        // Reduce
+
+        Map<String, Double> marks = new HashMap<>();
+        marks.put("Science",66.0);
+        marks.put("Maths",78.0);
+        marks.put("English",90.0);
+
+        OptionalDouble average = marks.values()
+                .stream()
+                .mapToDouble(m -> m)
+                .average();
+
+        System.out.println(average.getAsDouble());
+
+    }
+}
