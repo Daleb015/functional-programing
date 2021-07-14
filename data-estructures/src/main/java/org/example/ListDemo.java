c7d93f97f6d2dbf9183eca026f1db705caff5721
daleb <daleb015@gmail.com>
2021-07-13 23:11:19 -0500

    Data structures List

---
+++ data-estructures/src/main/java/org/example/ListDemo.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+
+        ListFun<Integer> list = ListFun.list(3,6,9);
+        list.forEach(System.out::println);
+
+        ListFun<Integer> addEle = list.addEle(56);
+        addEle.forEach(System.out::println);
+
+        ListFun<Integer> removeEle = addEle.removeEle(9);
+        removeEle.forEach(System.out::println);
+
+        ListFun<Integer> reverseList = removeEle.reverseList();
+        reverseList.forEach(System.out::println);
+
+        ListFun<Integer> list1 = ListFun.list(3,6,9,8);
+        ListFun<Integer> list2 = ListFun.list(37,78,90);
+
+        ListFun.concat(list1,list2).forEach(System.out::println);
+
+        List<Integer> l = List.of(2,5,8,6);
+        list1.addAllEle(l).forEach(System.out::println);
+
+    }
+}
